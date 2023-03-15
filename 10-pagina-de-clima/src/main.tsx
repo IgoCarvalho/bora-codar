6f7dc71f2df56ab5737b7944d86defe59dbd4b99
Igo Carvalho <igocarvalho00@gmail.com>
2023-03-14 23:39:14 -0300

    💄 add sass

--- 10-pagina-de-clima/src/main.tsx
+++ 10-pagina-de-clima/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import { App } from './App';
 
-import './styles/main.css';
+import './styles/main.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
